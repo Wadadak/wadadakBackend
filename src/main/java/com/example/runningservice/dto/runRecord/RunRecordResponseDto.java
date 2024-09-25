431b453d1b73a9429162bf63f121473d69a387e6
Kang Yeong Woo <rkdduddn8812@naver.com>
2024-09-25 09:45:02 +0900

    fix RunRecord에서 빌더패턴 및 req/resp dto로 toEntity FromEntity 메소드를 이동하여 CRUD 구조 변경

--- src/main/java/com/example/runningservice/dto/runRecord/RunRecordResponseDto.java
+++ src/main/java/com/example/runningservice/dto/runRecord/RunRecordResponseDto.java
@@ -1,6 +1,9 @@
 package com.example.runningservice.dto.runRecord;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import com.example.runningservice.entity.RunRecordEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,7 +16,20 @@ public class RunRecordResponseDto {
     private Integer runningTime;
     private Integer pace;
     private Integer runCount;
-    private LocalDateTime runningDate;
+    private LocalDate runningDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public static RunRecordResponseDto fromEntity(RunRecordEntity entity) {
+        return RunRecordResponseDto.builder()
+                .id(entity.getId())
+                .userId(entity.getUserId().getId())
+                .distance(entity.getDistance())
+                .runningTime(entity.getRunningTime())
+                .pace(entity.getPace())
+                .runningDate(entity.getRunningDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
 }
